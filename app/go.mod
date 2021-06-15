62fe98e8777ff539501d6b5955be1c919e94731c
Furkan Akyol <furkanakyol1997@hotmail.com>
2021-06-15 11:57:19 +0300

    index update fix

--- app/go.mod
+++ app/go.mod
@@ -4,11 +4,12 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
-	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.5.1
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
 	gorm.io/driver/postgres v1.1.0
-	gorm.io/driver/sqlite v1.1.4
+	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.21.9
+	gorm.io/plugin/soft_delete v1.0.1
 )
--- app/models/User.go
+++ app/models/User.go
@@ -1,11 +1,11 @@
 package models
 
-import "gorm.io/gorm"
+import "gorm.io/plugin/soft_delete"
 
 type User struct {
-	ID        uint           `json:"id" gorm:"primary_key"`
-	Name      string         `json:"name"`
-	Email     string         `json:"email" gorm:"uniqueIndex:udx_email"`
-	Password  string         `json:"password"`
-	DeletedAt gorm.DeletedAt `gorm:"uniqueIndex:udx_email"`
+	ID        uint                  `json:"id" gorm:"primary_key"`
+	Name      string                `json:"name"`
+	Email     string                `json:"email" gorm:"uniqueIndex:udx_email"`
+	Password  string                `json:"password"`
+	DeletedAt soft_delete.DeletedAt `gorm:"uniqueIndex:udx_email"`
 }
