5560be888210207ca3521347ebfb9a6f0264bafb
Furkan Akyol <furkanakyol1997@hotmail.com>
2021-06-15 11:41:48 +0300

    index update

--- app/models/User.go
+++ app/models/User.go
@@ -3,9 +3,9 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-	ID     uint   `json:"id" gorm:"primary_key"`
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uint           `json:"id" gorm:"primary_key"`
+	Name      string         `json:"name"`
+	Email     string         `json:"email" gorm:"uniqueIndex:udx_email"`
+	Password  string         `json:"password"`
+	DeletedAt gorm.DeletedAt `gorm:"uniqueIndex:udx_email"`
 }
